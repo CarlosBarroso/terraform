472f0a909a8dfde3ed79392ed16a4594a328415e
carlos barroso <cbc_sg@hotmail.com>
2020-09-22 10:13:36 +0200

    remove commented lines

--- aws/deploy infrastructure/infrastructura.tf
+++ aws/deploy infrastructure/infrastructura.tf
@@ -110,25 +110,6 @@ resource "aws_subnet" "subnet" {
   
   tags = merge (local.common_tags, {Name = "${var.environment_tag}-subnet${count.index + 1}"})
 }
-
-
-# resource "aws_subnet" "subnet1" {
-#   cidr_block              = var.subnet1_address_space
-#   vpc_id                  = aws_vpc.vpc.id
-#   map_public_ip_on_launch = "true"
-#   availability_zone       = data.aws_availability_zones.available.names[0]
-#   
-#   tags = merge (local.common_tags, {Name = "${var.environment_tag}-subnet1"})
-# }
-# 
-# resource "aws_subnet" "subnet2" {
-#   cidr_block              = var.subnet2_address_space
-#   vpc_id                  = aws_vpc.vpc.id
-#   map_public_ip_on_launch = "true"
-#   availability_zone       = data.aws_availability_zones.available.names[1]
-#   
-#   tags = merge (local.common_tags, {Name = "${var.environment_tag}-subnet2"})
-# }
   
 # routing #
 resource "aws_route_table" "rtb" {
@@ -148,18 +129,6 @@ resource "aws_route_table_association" "rta-subnet" {
   route_table_id = aws_route_table.rtb.id
   
 }
-
-# resource "aws_route_table_association" "rta-subnet1" {
-#   subnet_id      = aws_subnet.subnet1.id
-#   route_table_id = aws_route_table.rtb.id
-#   
-# }
-# 
-# resource "aws_route_table_association" "rta-subnet2" {
-#   subnet_id      = aws_subnet.subnet2.id
-#   route_table_id = aws_route_table.rtb.id
-#    
-# }
 
 # security groups #
 # load balancer security group
@@ -229,22 +198,6 @@ resource "aws_elb" "web" {
   
   tags = merge (local.common_tags, {Name = "${var.environment_tag}-elb-web"})
 }
-# resource "aws_elb" "web" {
-#   name            ="nginx-elb"
-#   subnets         = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
-#   security_groups = [aws_security_group.elb-sg.id]
-#   instances       = [aws_instance.nginx1.id, aws_instance.nginx2.id]
-# #  instances       = [aws_instance.nginx2.id]
-#   
-#   listener {
-#     instance_port     = 80
-# 	instance_protocol = "http"
-# 	lb_port           = 80
-# 	lb_protocol       = "http"
-#   }
-#   
-#   tags = merge (local.common_tags, {Name = "${var.environment_tag}-elb-web"})
-# }
 
 # instance
    resource "aws_instance" "nginx" {
@@ -314,143 +267,6 @@ resource "aws_elb" "web" {
   
      tags = merge (local.common_tags, {Name = "${var.environment_tag}-nginx${count.index + 1}"})
    }
-
-
-#   resource "aws_instance" "nginx1" {
-#     ami                    = data.aws_ami.aws-linux.id
-#     instance_type          = "t2.micro"
-#     subnet_id              = aws_subnet.subnet1.id
-#     key_name               = var.key_name
-#     vpc_security_group_ids = [aws_security_group.nginx-sg.id]
-#     iam_instance_profile   = aws_iam_instance_profile.nginx_profile.name
-#      
-#    
-#     connection {
-#       type        = "ssh"
-#   	host        = self.public_ip
-#   	user        = "ec2-user"
-#   	private_key = file (var.private_key_path)
-#     }
-#    
-#     provisioner "file" {
-#      content = <<EOF
-#  access_key =
-#  secret_key =
-#  security_token =
-#  use_https = True
-#  bucket_location = UE
-#  
-#  EOF
-#      destination = "/home/ec2-user/.s3cfg"
-#    }
-#    
-#    provisioner "file" {
-#      content = <<EOF
-#  /var/log/nginx/*log {
-#      daily
-#      rotate 10
-#      missingok
-#      compress
-#      sharedscripts
-#      postrotate
-#      endscript
-#      lastaction
-#          INSTANCE_ID=`curl --silent http://169.254.169.254/latest/meta-data/instance-id`
-#          sudo /usr/local/bin/s3cmd sync --config=/home/ec2-user/.s3cfg /var/log/nginx/ s3://${aws_s3_bucket.web_bucket.id}/nginx/$INSTANCE_ID/
-#      endscript
-#  }
-#  EOF
-#      destination = "/home/ec2-user/nginx"
-#    }
-#    
-#    provisioner "remote-exec" {
-#      inline = [
-#        "sudo yum install nginx -y",
-#        "sudo service nginx start",
-#        "sudo cp /home/ec2-user/.s3cfg /root/.s3cfg",
-#        "sudo cp /home/ec2-user/nginx /etc/logrotate.d/nginx",
-#  	  "sudo yum -y install dos2unix",
-#  	  "sudo dos2unix /etc/logrotate.d/nginx",
-#  	  "sudo pip install --upgrade pip",
-#        "sudo /usr/local/bin/pip install s3cmd",
-#        "s3cmd get s3://${aws_s3_bucket.web_bucket.id}/website/index.html .",
-#        "s3cmd get s3://${aws_s3_bucket.web_bucket.id}/website/Globo_logo_Vert.png .",
-#        "sudo cp /home/ec2-user/index.html /usr/share/nginx/html/index.html --remove-destination",
-#        "sudo cp /home/ec2-user/Globo_logo_Vert.png /usr/share/nginx/html/Globo_logo_Vert.png --remove-destination",
-#        "sudo logrotate -f /etc/logrotate.conf"  
-#      ]
-#    }
-#  
-#     tags = merge (local.common_tags, {Name = "${var.environment_tag}-nginx1"})
-#   }
-# 
-# resource "aws_instance" "nginx2" {
-#   ami                    = data.aws_ami.aws-linux.id
-#   instance_type          = "t2.micro"
-#   subnet_id              = aws_subnet.subnet2.id
-#   key_name               = var.key_name
-#   vpc_security_group_ids = [aws_security_group.nginx-sg.id]
-#   iam_instance_profile   = aws_iam_instance_profile.nginx_profile.name
-#   
-#   connection {
-#     type        = "ssh"
-# 	host        = self.public_ip
-# 	user        = "ec2-user"
-# 	private_key = file (var.private_key_path)
-#   }
-#   
-#   provisioner "file" {
-#     content = <<EOF
-# access_key =
-# secret_key =
-# security_token =
-# use_https = True
-# bucket_location = UE
-# 
-# EOF
-#     destination = "/home/ec2-user/.s3cfg"
-#   }
-#   
-#   provisioner "file" {
-#     content = <<EOF
-# /var/log/nginx/*log {
-#     daily
-#     rotate 10
-#     missingok
-#     compress
-#     sharedscripts
-#     postrotate
-#     endscript
-#     lastaction
-#         INSTANCE_ID=`curl --silent http://169.254.169.254/latest/meta-data/instance-id`
-#         sudo /usr/local/bin/s3cmd sync --config=/home/ec2-user/.s3cfg /var/log/nginx/ s3://${aws_s3_bucket.web_bucket.id}/nginx/$INSTANCE_ID/
-#     endscript
-# }
-# EOF
-#     destination = "/home/ec2-user/nginx"
-#   }
-#   
-#   provisioner "remote-exec" {
-#     inline = [
-#       "sudo yum install nginx -y",
-#       "sudo service nginx start",
-#       "sudo cp /home/ec2-user/.s3cfg /root/.s3cfg",
-#       "sudo cp /home/ec2-user/nginx /etc/logrotate.d/nginx",
-# 	  "sudo yum -y install dos2unix",
-# 	  "sudo dos2unix /etc/logrotate.d/nginx",
-# 	  "sudo pip install --upgrade pip",
-#       "sudo /usr/local/bin/pip install s3cmd",
-#       "s3cmd get s3://${aws_s3_bucket.web_bucket.id}/website/index.html .",
-#       "s3cmd get s3://${aws_s3_bucket.web_bucket.id}/website/Globo_logo_Vert.png .",
-#       "sudo cp /home/ec2-user/index.html /usr/share/nginx/html/index.html --remove-destination",
-#       "sudo cp /home/ec2-user/Globo_logo_Vert.png /usr/share/nginx/html/Globo_logo_Vert.png --remove-destination",
-#       "sudo logrotate -f /etc/logrotate.conf"  
-#     ]
-#   }
-# 
-# 
-#   tags = merge (local.common_tags, {Name = "${var.environment_tag}-nginx2"})
-# }
 
 # S3 Bucket config#
 resource "aws_iam_role" "allow_nginx_s3" {
