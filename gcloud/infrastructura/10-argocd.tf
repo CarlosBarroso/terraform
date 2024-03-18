18c41ba11de2093ba07b9e3f8d8a7b5e0855095b
Carlos <cbc_sg@yahoo.es>
2024-03-18 09:39:14 +0000

    modify argo application to deploy jenkins

--- gcloud/infrastructura/10-argocd.tf
+++ gcloud/infrastructura/10-argocd.tf
@@ -3,23 +3,19 @@ variable "namespace_argocd" {
   default = "argocd"
 }
 
-resource "kubernetes_namespace" "argocd" {
-  metadata {
-    name = var.namespace_argocd
-  }
-}
-
 resource "helm_release" "argocd-staging" {
   name       = "argocd-staging"
   chart      = "argo-cd"
   repository = "https://argoproj.github.io/argo-helm"
   version    = "5.27.3"
   namespace  = var.namespace_argocd
+  create_namespace = true  
   timeout    = "1200"
   values     = [templatefile("./argocd/install.yaml", {})]
 }
 
 resource "null_resource" "password" {
+  depends_on = [helm_release.argocd-staging]
   provisioner "local-exec" {
     working_dir = "./argocd"
     command     = "kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath={.data.password} | base64 -d > argocd-login.txt"
